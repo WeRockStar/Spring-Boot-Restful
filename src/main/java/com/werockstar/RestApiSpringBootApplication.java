004cba2ba95f735cc942447be8e70e0eb76bf208
WeRockStar <kotchaphan.m@gmail.com>
2017-01-23 23:21:38 +0700

    Initial commit

---
+++ src/main/java/com/werockstar/RestApiSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.werockstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringBootApplication.class, args);
+	}
+}
